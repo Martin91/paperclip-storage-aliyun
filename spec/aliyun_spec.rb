742fc55b072d7eb11824fbb01d41d467d571566a
Martin Hong <hongzeqin@gmail.com>
2015-08-02 10:12:56 +0800

    update rspec code to the newest 3.3.0 syntax

--- spec/aliyun_spec.rb
+++ spec/aliyun_spec.rb
@@ -11,14 +11,16 @@ describe Aliyun::Connection do
   describe '#put' do
     it "upload the attachment" do
       url = @connection.put @path, load_attachment("girl.jpg")
-      Net::HTTP.get_response(URI.parse(url)).code.should == "200"
+      response_code = Net::HTTP.get_response(URI.parse(url)).code
+      expect(response_code).to eq("200")
     end
   end
 
   describe '#delete' do
     it "delete the attachment" do
       url = @connection.delete @path
-      Net::HTTP.get_response(URI.parse(url)).code.should == "404"
+      response_code = Net::HTTP.get_response(URI.parse(url)).code
+      expect(response_code).to eq("404")
     end
   end
 
@@ -27,13 +29,13 @@ describe Aliyun::Connection do
       @connection.put @path, load_attachment("girl.jpg")
     end
 
-    it "should return true if the file has been uploaded" do
-      @connection.exists?(@path).should be_true
+    it "return true if the file has been uploaded" do
+      expect(@connection.exists?(@path)).to be_truthy
     end
 
-    it "should return false if the specified file didn't exist" do
+    it "return false if the specified file didn't exist" do
       @connection.delete @path
-      @connection.exists?(@path).should be_false
+      expect(@connection.exists?(@path)).to be_falsey
     end
   end
 end
