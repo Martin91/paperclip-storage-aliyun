e9c4e90401dd37c5d7e6542e3e37a2d2b998b13f
Martin Hong <hongzeqin@gmail.com>
2015-08-02 12:22:23 +0800

    rewrite documents through yard

--- lib/aliyun/connection.rb
+++ lib/aliyun/connection.rb
@@ -22,6 +22,9 @@ module Aliyun
       @aliyun_host = options[:host] || @aliyun_upload_host
     end
 
+    # the file host according to the connection configurations
+    #
+    # @return [String] the host value 
     def fetch_file_host
       @aliyun_host
     end
@@ -32,16 +35,18 @@ module Aliyun
     # @param path [String] the path of file storaged in Aliyun OSS
     # @return [Hash] the meta data of the file
     # @note the example headers will be like:
+    #
+    #   {
     #    {:date=>"Sun, 02 Aug 2015 02:42:45 GMT",
-    #     :content_type=>"image/jpg",
-    #     :content_length=>"125198",
-    #     :connection=>"close",
-    #     :accept_ranges=>"bytes",
-    #     :etag=>"\"336262A42E5B99AFF5B8BC66611FC156\"",
-    #     :last_modified=>"Sun, 01 Dec 2013 16:39:57 GMT",
-    #     :server=>"AliyunOSS",
-    #     :x_oss_object_type=>"Normal",
-    #     :x_oss_request_id=>"55BD83A5D4C05BDFF4A329E0"}
+    #    :content_type=>"image/jpg",
+    #    :content_length=>"125198",
+    #    :connection=>"close",
+    #    :accept_ranges=>"bytes",
+    #    :etag=>"\"336262A42E5B99AFF5B8BC66611FC156\"",
+    #    :last_modified=>"Sun, 01 Dec 2013 16:39:57 GMT",
+    #    :server=>"AliyunOSS",
+    #    :x_oss_object_type=>"Normal",
+    #    :x_oss_request_id=>"55BD83A5D4C05BDFF4A329E0"}}
     #
     def head(path)
       url = path_to_url(path)
@@ -50,18 +55,16 @@ module Aliyun
       {}
     end
 
-=begin rdoc
-上传文件
-
-== 参数:
-- path - remote 存储路径
-- file - 需要上传文件的 File 对象
-- options:
-  - content_type - 上传文件的 MimeType，默认 `image/jpg`
-
-== 返回值:
-图片的下载地址
-=end
+    # Upload File to Aliyun OSS
+    # https://docs.aliyun.com/#/pub/oss/api-reference/object&PutObject
+    # 
+    # @param path [String] the target storing path on the oss
+    # @param file [File] an instance of File represents a file to be uploaded
+    # @param options [Hash]
+    #   - content_type - MimeType value for the file, default is "image/jpg"
+    # 
+    # @return [String] The downloadable url of the uploaded file
+    # @return [nil] if the uploading failed
     def put(path, file, options={})
       path = format_path(path)
       bucket_path = get_bucket_path(path)
@@ -82,15 +85,12 @@ module Aliyun
       response.code == 200 ? path_to_url(path) : nil
     end
 
-=begin rdoc
-删除 Remote 的文件
-
-== 参数:
-- path - remote 存储路径
-
-== 返回值:
-图片的下载地址
-=end
+    # Delete a file from the OSS
+    # https://docs.aliyun.com/#/pub/oss/api-reference/object&DeleteObject
+    #
+    # @param path [String] the path to retrieve the file on remote storage
+    # @return [String] the expired url to the file, if the file deleted successfully
+    # @return [nil] if the delete operation failed
     def delete(path)
       path = format_path(path)
       bucket_path = get_bucket_path(path)
@@ -105,15 +105,11 @@ module Aliyun
       response.code == 204 ? url : nil
     end
 
-=begin rdoc
-下载 Remote 的文件
-
-== 参数:
-- path - remote 存储路径
-
-== 返回值:
-请求的图片的数据流
-=end
+    # Download the file from OSS
+    # https://docs.aliyun.com/#/pub/oss/api-reference/object&GetObject
+    #
+    # @param path [String] the path to retrieve the file on remote storage
+    # @return [?] the file content consist of bytes
     def get(path)
       path = format_path(path)
       bucket_path = get_bucket_path(path)
@@ -128,25 +124,28 @@ module Aliyun
       response.body
     end
 
-=begin rdoc
-检查远程服务器是否已存在指定文件
-
-== 参数:
-- path - remote 存储路径
-
-== 返回值:
-true/false
-=end
+    # Determine if the file exists on the OSS
+    # https://docs.aliyun.com/#/pub/oss/api-reference/object&HeadObject
+    #
+    # @param path [String] the path to retrieve the file on remote storage
+    # @return [true] if file exists
+    # @return [false] if file could not be found
     def exists?(path)
       head(path).empty? ? false : true
     end
 
-    ##
-    # 阿里云需要的 GMT 时间格式
+    # The GMT format time referenced from HTTP 1.1
+    # https://docs.aliyun.com/#/pub/oss/api-reference/public-header
+    #
+    # @return [String] a string represents the formated time, e.g. "Wed, 05 Sep. 2012 23:00:00 GMT"
     def gmtdate
       Time.now.gmtime.strftime("%a, %d %b %Y %H:%M:%S GMT")
     end
 
+    # remove leading slashes in the path
+    #
+    # @param path [String] the path to retrieve the file on remote storage
+    # @return [String] the new string after removing leading slashed
     def format_path(path)
       return "" if path.blank?
       path.gsub!(/^\/+/,"")
@@ -154,18 +153,32 @@ true/false
       path
     end
 
+    # A path consis of the bucket name and file name
+    # https://docs.aliyun.com/#/pub/oss/api-reference/access-control&signature-header
+    #
+    # @param path [String] the path to retrieve the file on remote storage
+    # @return [String] the expected bucket path, e.g. "test-bucket/oss-api.pdf"
     def get_bucket_path(path)
       [@aliyun_bucket,path].join("/")
     end
 
-    ##
-    # 根据配置返回完整的上传文件的访问地址
+    # The full path contains host name to the file
+    #
+    # @param path [String] the path to retrieve the file on remote storage
+    # @return [String] the expected full path, e.g. "http://martin-test.oss-cn-hangzhou.aliyuncs.com/oss-api.pdf"
     def path_to_url(path)
       return path if path =~ /^https?:\/{2}/  # 已经是全路径
       "http://#{fetch_file_host}/#{path}"
     end
 
     private
+    # The signature algorithm
+    # https://docs.aliyun.com/#/pub/oss/api-reference/access-control&signature-header
+    #
+    # @param verb [String] the request verb, e.g. "GET" or "DELETE"
+    # @param content_md5 [String] the md5 value for the content to be uploaded
+    # @param content_type [String] the content type of the file, e.g. "application/pdf"
+    # @param date [String] the GMT formatted date string
     def sign(verb, path, content_md5 = '', content_type = '', date)
       canonicalized_oss_headers = ''
       canonicalized_resource = "/#{path}"
