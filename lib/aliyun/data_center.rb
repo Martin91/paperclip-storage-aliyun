8e9621df21768289b7066682104f756e66c9c7cc
Ahmad Abdel-Yaman <a.yaman@gmail.com>
2017-02-08 15:20:23 +0200

    Cleanup method for finding data center

--- lib/aliyun/data_center.rb
+++ lib/aliyun/data_center.rb
@@ -20,20 +20,15 @@ module Aliyun
     )
 
     def get_endpoint(options)
-      data_center = find_center(options[:data_center])
+      data_center = options[:data_center]
 
-      unless data_center && AVAILABLE_DATA_CENTERS.include?(data_center)
+      data_center.prepend('oss-') unless data_center.match(/^oss/)
+
+      unless AVAILABLE_DATA_CENTERS.include?(data_center)
         fail InvalildDataCenter, "Unsupported Data Center #{options[:data_center]} Detected"
       end
 
       "#{data_center}#{options[:internal] ? '-internal' : ''}.aliyuncs.com"
-    end
-
-    def find_center(data_center)
-      return if /^(oss|cn|us|ap|oss-cn)$/.match(data_center)
-
-      regexp = Regexp.new(data_center)
-      AVAILABLE_DATA_CENTERS.find { |center| regexp.match(center) }
     end
   end
 end
